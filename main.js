46181251b8ddade3e7234fa568f3a11e70e5e1a2
nicosimarro <72930254+nicosimarro@users.noreply.github.com>
2023-12-12 13:35:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,87 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Cambiar el color de fondo del encabezado al hacer clic en la foto
-    document.getElementById('profile-picture').addEventListener('click', function () {
-        document.querySelector('header').classList.toggle('header-clicked');
-    });
+// main.js
 
-    // Mostrar/ocultar secciones al hacer clic en los botones
-    document.getElementById('habilidades').addEventListener('click', function () {
-        toggleSection('habilidades');
+// Navbar color change on scroll
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#mainNav').addClass('navbar-shrink');
+        } else {
+            $('#mainNav').removeClass('navbar-shrink');
+        }
     });
-
-    document.getElementById('experiencia').addEventListener('click', function () {
-        toggleSection('experiencia');
-    });
-
-    document.getElementById('educacion').addEventListener('click', function () {
-        toggleSection('educacion');
-    });
-
-    document.getElementById('proyectos').addEventListener('click', function () {
-        toggleSection('proyectos');
-    });
-
-    document.getElementById('certificaciones').addEventListener('click', function () {
-        toggleSection('certificaciones');
-    });
-
-    document.getElementById('idiomas').addEventListener('click', function () {
-        toggleSection('idiomas');
-    });
-
-    // Función para mostrar/ocultar secciones
-    function toggleSection(sectionId) {
-        var section = document.getElementById(sectionId);
-        section.style.display = (section.style.display === 'none' || section.style.display === '') ? 'block' : 'none';
-    }
 });
+
+// Smooth scrolling using jQuery easing
+$(document).ready(function () {
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate(
+                    {
+                        scrollTop: target.offset().top - 70,
+                    },
+                    1000,
+                    'easeInOutExpo'
+                );
+                return false;
+            }
+        }
+    });
+});
+
+// Typewriter effect
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+        this.txtElement = txtElement;
+        this.words = words;
+        this.txt = '';
+        this.wordIndex = 0;
+        this.wait = parseInt(wait, 10);
+        this.type();
+        this.isDeleting = false;
+    }
+
+    type() {
+        const current = this.wordIndex % this.words.length;
+        const fullTxt = this.words[current];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.txtElement.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+        let typeSpeed = 200;
+
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            typeSpeed = this.wait;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.wordIndex++;
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const txtElement = document.querySelector('.typewrite');
+    const words = JSON.parse(txtElement.getAttribute('data-type'));
+    const wait = txtElement.getAttribute('data-period');
+    new TypeWriter(txtElement, words, wait);
+}
