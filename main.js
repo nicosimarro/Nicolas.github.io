5a2d42dad6c6084212e9ce491199746ad9225fb9
nicosimarro <72930254+nicosimarro@users.noreply.github.com>
2023-12-15 10:26:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,87 +1,24 @@
-// main.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Desplazamiento suave para enlaces de la barra de navegación
+    document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 
-// Navbar color change on scroll
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#mainNav').addClass('navbar-shrink');
+    // Funcionalidad adicional para la barra de navegación
+    // Por ejemplo, cambiar el aspecto al desplazarse
+    window.addEventListener('scroll', function() {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
         } else {
-            $('#mainNav').removeClass('navbar-shrink');
+            navbar.classList.remove('scrolled');
         }
     });
+
+    // Aquí puedes agregar más funcionalidades según sea necesario
 });
-
-// Smooth scrolling using jQuery easing
-$(document).ready(function () {
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    'easeInOutExpo'
-                );
-                return false;
-            }
-        }
-    });
-});
-
-// Typewriter effect
-class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-        this.txtElement = txtElement;
-        this.words = words;
-        this.txt = '';
-        this.wordIndex = 0;
-        this.wait = parseInt(wait, 10);
-        this.type();
-        this.isDeleting = false;
-    }
-
-    type() {
-        const current = this.wordIndex % this.words.length;
-        const fullTxt = this.words[current];
-
-        if (this.isDeleting) {
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.txtElement.innerHTML = `<span class="wrap">${this.txt}</span>`;
-
-        let typeSpeed = 200;
-
-        if (this.isDeleting) {
-            typeSpeed /= 2;
-        }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-            typeSpeed = this.wait;
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.wordIndex++;
-            typeSpeed = 500;
-        }
-
-        setTimeout(() => this.type(), typeSpeed);
-    }
-}
-
-document.addEventListener('DOMContentLoaded', init);
-
-function init() {
-    const txtElement = document.querySelector('.typewrite');
-    const words = JSON.parse(txtElement.getAttribute('data-type'));
-    const wait = txtElement.getAttribute('data-period');
-    new TypeWriter(txtElement, words, wait);
-}
